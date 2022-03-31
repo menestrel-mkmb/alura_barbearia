8c24ed574a23b152c26f1c27e16e7e2635915f2f
Michael Kevin <michaelkmb@hotmail.com>
2022-03-31 11:39:03 -0300

    intro js;

---
+++ javascript/4-conversaoDeTipos.js
@@ -0,0 +1,9 @@
+console.log("Conversão de Tipos");
+
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));
+
+console.log("10" / "2");
+console.log("Ricardo" / "2"); //NaN
+
+console.log(6.5 + 6,5);
---
+++ javascript/5-atribuicaoDeVariaveis.js
@@ -0,0 +1,10 @@
+console.log("Atribuição de variáveis");
+
+const idade = 27;
+let nome = "Michael Kevin";
+const sobrenome = "Medeiros Barbosa";
+
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+//nome => nomecompleto
+nome = nome + sobrenome;
