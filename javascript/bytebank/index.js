5ad47c93a603dfac3e5e7bf424f44e41ae89feaa
Michael Kevin <michaelkmb@hotmail.com>
2022-04-01 21:43:09 -0300

    fixed .js from imported;

--- javascript/bytebank/index.js
+++ javascript/bytebank/index.js
@@ -1,5 +1,5 @@
 import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./ContaCorrente";
+import { ContaCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
 const cliente2 = new Cliente();
