7576f078e207702e737d009913ea59b60ca39706
Michael Kevin <michaelkmb@hotmail.com>
2022-04-01 21:57:35 -0300

    CC methods;

--- javascript/bytebank/ContaCorrente.js
+++ javascript/bytebank/ContaCorrente.js
@@ -1,6 +1,9 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
     agencia;
-    #saldo;
+    #saldo = 0;
+    cliente;
 
     sacar(valor){
         if(!(typeof(valor) == "number") || valor <= 0) return;
@@ -20,8 +23,17 @@ export class ContaCorrente {
         return valor;
     }
 
+    transferir(valor, conta){
+        return conta.depositar(this.sacar(valor));
+    }
+
     verSaldo(){
         return this.#saldo;
+    }
+
+    definirTitularidade(novoTitular){
+        this.cliente = novoTitular;
+        return true;
     }
 }
 
--- javascript/bytebank/index.js
+++ javascript/bytebank/index.js
@@ -10,11 +10,21 @@ cliente1.cpf = 11122233309;
 
 const contaCorrente1 = new ContaCorrente();
 contaCorrente1.agencia = 1001;
-contaCorrente1.saldo = 0;
-
-console.log(cliente1);
+contaCorrente1.depositar(200);
+contaCorrente1.definirTitularidade(cliente1);
 
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
-console.log(cliente2);
+const contaCorrente2 = new ContaCorrente();
+contaCorrente2.agencia = 1001;
+contaCorrente2.depositar(100);
+contaCorrente2.definirTitularidade(cliente2);
+
+console.log(contaCorrente1.verSaldo());
+console.log(contaCorrente2.verSaldo());
+
+contaCorrente1.transferir(50, contaCorrente2);
+
+console.log(contaCorrente1.verSaldo());
+console.log(contaCorrente2.verSaldo());
