d28471ece439a75108dc8b0e9c17e1e11f6c47be
Michael <michaelkmb@hotmail.com>
2022-07-09 21:09:33 -0300

    feat sub-menu on mediaQuery; partial change events on widthSize;

--- alura_books/js/main.js
+++ alura_books/js/main.js
@@ -1,4 +1,5 @@
 const hamburguerMenu = document.querySelector(".menu__ul");
+const categoriesSpan = document.querySelector(".categories__span");
 
 let openHamburguerMenu = (event) => {
   event.preventDefault();
@@ -8,4 +9,23 @@ let openHamburguerMenu = (event) => {
   subMenu.classList.toggle("show-sub-menu");
 };
 
-hamburguerMenu.addEventListener("click", openHamburguerMenu);
+let openCategories = (event) => {
+  event.preventDefault();
+
+  const subMenu = document.querySelector(".sub-menu");
+  subMenu.classList.toggle("hidden");
+};
+
+window.onresize = function () {
+  event.preventDefault();
+
+  console.log(event.target);
+
+  if (event.target < 1024) {
+    hamburguerMenu.addEventListener("click", openHamburguerMenu);
+    categoriesSpan.removeEventListener("click", openHamburguerMenu);
+  } else {
+    categoriesSpan.addEventListener("click", openHamburguerMenu);
+    hamburguerMenu.removeEventListener("click", openHamburguerMenu);
+  }
+};
