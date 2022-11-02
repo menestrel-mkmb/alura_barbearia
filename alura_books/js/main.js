65743fd15d7c10728fc4140ca6e1b4d805e534f8
Michael <michaelkmb@hotmail.com>
2022-11-02 12:16:14 -0300

    script to responsive swipper init;

--- alura_books/js/main.js
+++ alura_books/js/main.js
@@ -1,3 +1,4 @@
+let slidesNumber = 3;
 const hamburguerMenu = document.querySelector(".menu__ul");
 const categoriesMenu = document.querySelector(".categories");
 
@@ -18,15 +19,40 @@ let openCategories = (event) => {
 
 window.onresize = function () {
   changeEvents();
+  // changeSlidesPerView();
 };
 
 let changeEvents = () => {
   if (window.innerWidth < 1024) {
     hamburguerMenu.addEventListener("click", openHamburguerMenu);
     categoriesMenu.removeEventListener("click", openHamburguerMenu);
+    window.innerWidth > 480 ? (slidesNumber = 2) : (slidesNumber = 1);
   } else {
     categoriesMenu.addEventListener("click", openHamburguerMenu);
     hamburguerMenu.removeEventListener("click", openHamburguerMenu);
+    slidesNumber = 3;
   }
 };
+
 changeEvents();
+
+//slider
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: slidesNumber,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+let changeSlidesPerView = () => {
+  const slides = document.querySelectorAll(".swiper");
+};
